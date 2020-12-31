7df7e50986c154b9b31a852fc51d277722039b97
TheEF <eldar.ikhsanov@mail.ru>
2020-12-31 15:38:36 +0500

    Created tranzaction system

--- bank-reg-beta.cpp
+++ bank-reg-beta.cpp
@@ -3,43 +3,54 @@ using namespace std;
 
 int users[100];
 int usrdata[100];
-int nxtusr = 0;
+int nxtusr = 1;
 int session;
+users[0] = 572457; 
 
-void login()
+void devtools();
+
+int tranzactions();
+
+void login(int user)
 {
     int reglog;
     int pass = 0;
     int idishnik;
-    cout << "Login or Register?" << endl;
-    cout << "1: Login 2:Register" << endl;
-    cin >> reglog;
-    if(reglog == 1)
+    if(user == 0)
     {
-       cout << "Enter your password (only numbers plz): ";
-       cin >> pass;
-       cout << "\nEnter your ID: ";
-       cin >> idishnik;
-       if(pass == users[idishnik])
-       {
-          cout << "You logined";
-          session = idishnik;
-       }
+    	session == user;
+    }
+    else {
+    	cout << "Login or Register?" << endl;
+    	cout << "1: Login 2:Register" << endl;
+    	cin >> reglog;
+    	if(reglog == 1)
+    	{	
+       		cout << "Enter your password (only numbers plz): ";
+       		cin >> pass;
+       		cout << "\nEnter your ID: ";
+       		cin >> idishnik;
+       		if(pass == users[idishnik])
+       		{	
+          		cout << "You logined";
+          		session = idishnik;
+        }
        else {
           cout << "Wrong password or ID." << endl;
           login();
        }
-    } 
-    else {
-       cout << "Enter your new password: ";
-       cin >> pass;
-       users[nxtusr] = pass;
-       cout << "You registered, please login" << endl;
-       cout << "Your id is " << nxtusr << endl;
-       nxtusr++;
-       login();
     }
-}
+    else 
+    {
+       		cout << "Enter your new password: ";
+       		cin >> pass;
+       		users[nxtusr] = pass;
+       		cout << "You registered, please login" << endl;
+       		cout << "Your id is " << nxtusr << endl;
+       		nxtusr++;
+       		login();
+    	}
+    }
 
 int main()
 {
@@ -49,18 +60,89 @@ int main()
     {
        cout << "\nWelcome, " << session << endl;
        cout << "Money is " << usrdata[session] << endl;
-       cout << "1: Write manes" << endl;
-       cout << "2: Exit" << endl;
+       cout << "1: Tranzactions" << endl;
+       cout << "2: Games (coming soon)" << endl;
+       cout << "5: Exit" << endl;
+       cout << "7: Dev-tools" << endl;
        cin >> chosik;
        switch (chosik)
        {   
           case 1:
-              cout << "Enter a number ";
-              cin >> usrdata[session];
+              tranzactions();
           break;
           case 2:
               login();
+	  break;
+	  case 7:
+	      devtools()
        }
     }
     return 0;
+}
+
+void devtools()
+{
+	int choose;
+	if(session == 0)
+	{
+		cout << "1: Write manes" << endl;
+		cout << "2: Usr list" << endl;
+		cin >> choose;
+		switch (choose) 
+		{
+			case 1:
+				int chid;
+				cout << "Enter a usrID" << endl;
+				cin >> chid;
+				if(users[chid] == 0)
+				{
+					cout << "User doesn't exist" << endl;	
+				}
+				else {
+					int amountmanes;
+					cout << "Enter a nuber" << endl;
+					cin >> amountmanes;
+					usrdata[chid] = amountmanes;
+					cout << "Sucess!" << endl;
+				}
+			break;
+			case 2:
+				
+		}
+	}
+}
+
+int tranzactions()
+{
+	int choose;
+	int chuser;
+	int amount;
+	cout << "1: Send money" << endl;
+	cout << "2: Take money" << endl;
+	cout << "Enter a number: ";
+	cin >> choose;
+	switch (choose)
+	{
+		case 1:
+			cout << "Enter a user ID";
+			cin >> chuser;
+			if(users[chuser] == 0)
+			{
+				cout << "User doesn't exist" << endl;
+				return 1;
+			}
+			else {
+				cout << "You want to transfer: ";
+				cin >> amount;
+				if(amount >= usrdata[session])
+				{
+					cout << "Not enough money" << endl; 
+				}
+				else {
+					usrdata[chuser] = usrdata[chuser] + amount;
+					return 0;
+				}
+			}
+	}
+
 }
